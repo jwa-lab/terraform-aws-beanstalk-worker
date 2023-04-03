ba5af68a47ed2f55ed0dbd83b1ad9043cfae9595
Thomas Klein <thomas.klein@jwalab.com>
2023-04-03 13:06:28 +0200

    removed unused locals

--- locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  worker_settings = setsubtract([
-    {
-      namespace = "aws:ec2:instances"
-      name = "InstanceTypes"
-      value = var.instance_type != null ? var.instance_type : (var.production ? "t4g.small" : "t4g.micro")
-    },
-  ], [{}])
-}
