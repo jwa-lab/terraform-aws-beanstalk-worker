2f14a4cb4c9e2c5680f88c0a2c3d21f10a9e5989
Thomas Klein <thomas.klein@jwalab.com>
2023-04-03 13:34:21 +0200

    added security group id as output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "instance_role" {
 output "beanstalk_env" {
   value = module.beanstalk.beanstalk_env
 }
+
+output "instances_security_group_id" {
+  value = module.beanstalk.instances_security_group_id
+}
